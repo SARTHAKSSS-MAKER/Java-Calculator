6a4e074d1e239168a6d99386c13e9bb06090c4ff
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-09 11:40:19 +0530

    Version 1: Basic calculator with arithmetic operations

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+    public static void main(String[] args) {
+
+        double num1 = 10.5;
+        double num2 = 5.0;
+
+        double addition = num1 + num2;
+        System.out.println("Addition: " + addition);
+
+        double subtraction = num1 - num2;
+        System.out.println("Subtraction: " + subtraction);
+
+        double multiplication = num1 * num2;
+        System.out.println("Multiplication: " + multiplication);
+
+        double division = num1 / num2;
+        System.out.println("Division: " + division);
+
+    }  
+}   
